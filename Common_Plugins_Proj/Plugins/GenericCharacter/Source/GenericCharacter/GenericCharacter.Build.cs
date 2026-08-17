cb0d057a8eb5d23d5de6cc1fdaec3a0536c141b5
E-Dawkins <ethandawkins@hotmail.co.uk>
2026-08-17 22:36:53 +1000

    Setup 'GC_GenericCharacter' class with WASD input

--- Common_Plugins_Proj/Plugins/GenericCharacter/Source/GenericCharacter/GenericCharacter.Build.cs
+++ Common_Plugins_Proj/Plugins/GenericCharacter/Source/GenericCharacter/GenericCharacter.Build.cs
@@ -26,6 +26,7 @@ public class GenericCharacter : ModuleRules
 			new string[]
 			{
 				"Core",
+                "EnhancedInput",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
--- Common_Plugins_Proj/Plugins/GenericCharacter/Source/GenericCharacter/Public/GC_GenericCharacter.h
+++ Common_Plugins_Proj/Plugins/GenericCharacter/Source/GenericCharacter/Public/GC_GenericCharacter.h
@@ -6,6 +6,13 @@
 #include "GameFramework/Character.h"
 #include "GC_GenericCharacter.generated.h"
 
+// Unreal classes
+class UCameraComponent;
+class UInputMappingContext;
+
+// GC classes
+class UGC_InputActions;
+
 UCLASS(meta = (DisplayName = "Generic Character"))
 class GENERICCHARACTER_API AGC_GenericCharacter : public ACharacter
 {
@@ -18,10 +25,43 @@ protected:
 	virtual void BeginPlay() override;
 
 public:
-	virtual void Tick(float DeltaTime) override;
-
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-public:
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category = "GenericCharacter|Input", meta = (DisplayName = "Input - Move"))
+	void Input_Move_BP(const FInputActionValue& Value);
+	void Input_Move_Native(const FInputActionValue& Value);
+
+protected:
+	UPROPERTY()
+	UCameraComponent* CameraComponent;
+
+protected:
+	// Default input mapping context to add in 'BeginPlay'
+	UPROPERTY(EditDefaultsOnly, Category = "GenericCharacter|Input", meta = (DisplayName = "Default IMC"))
+	UInputMappingContext* DefaultImc;
+
+	// Data asset containing all our input actions
+	UPROPERTY(EditDefaultsOnly, Category = "GenericCharacter|Input")
+	UGC_InputActions* InputActions;
 
 };
+
+#pragma region AssertLogic
+static void PrintDebugMessage(const FString& Msg) {
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Orange, Msg);
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
+}
+
+#define CHECK_VALID(x) \
+{ \
+	if (!IsValid(x)) \
+	{ \
+		PrintDebugMessage(FString::Format(TEXT("[{0}] [Line {1}] '{2}' is invalid on '{3}'!"), { __FUNCTION__, __LINE__, #x, UKismetSystemLibrary::GetDisplayName(this) })); \
+		return; \
+	} \
+}
+#pragma endregion
